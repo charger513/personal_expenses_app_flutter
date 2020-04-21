69c1788b101be616e91e06ed03dc085bc2a90707
Emmanuel Chargoy <charger513@hotmail.com>
2020-04-21 15:57:14 -0500

    Refactor appBar

--- lib/main.dart
+++ lib/main.dart
@@ -148,13 +148,8 @@ class _MyHomePageState extends State<MyHomePage> {
     ];
   }
 
-  @override
-  Widget build(BuildContext context) {
-    final mediaQuery = MediaQuery.of(context);
-
-    final isLandscape = mediaQuery.orientation == Orientation.landscape;
-
-    final PreferredSizeWidget appBar = Platform.isIOS
+  PreferredSizeWidget _buildAppBar() {
+    return Platform.isIOS
         ? CupertinoNavigationBar(
             middle: Text('Personal Expenses'),
             trailing: Row(
@@ -176,6 +171,15 @@ class _MyHomePageState extends State<MyHomePage> {
               ),
             ],
           );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final mediaQuery = MediaQuery.of(context);
+
+    final isLandscape = mediaQuery.orientation == Orientation.landscape;
+
+    final appBar = _buildAppBar();
 
     final txListWidget = Container(
       height: (mediaQuery.size.height -
