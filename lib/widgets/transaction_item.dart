4b231e5f0fcf6181ef7d20377ff1176e091e3a90
Emmanuel Chargoy <charger513@hotmail.com>
2020-04-21 15:39:20 -0500

    Using Builder Methods

--- lib/widgets/transaction_item.dart
+++ lib/widgets/transaction_item.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 import 'package:personal_expenses_app_flutter/models/transaction.dart';
 
 class TransactionItem extends StatelessWidget {
